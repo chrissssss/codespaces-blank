7fb4417b330db70438e74c8d031a5dad6687526c
chrissssss <ak_2001@gmx.de>
2022-11-24 15:26:15 +0000

    WIP

--- app/app.py
+++ app/app.py
@@ -1,12 +1,29 @@
 import subprocess
+import sys
 
 print("Python running!")
 
-user="test"
-host="mysshserver"
-cmd="date"
+user = "test"
+host = "mysshserver"
+cmd = "date"
 
-cmdline=f"ssh {user}@{host} {cmd}"
-subprocess.Popen(cmdline, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+cmdline = f"ssh -oStrictHostKeyChecking=no {user}@{host} {cmd}"
 
-print("Python finished!") 
+
+print("Python finished!")
+
+command = "uname -a"
+
+# ["ssh", "some-host@some-user", "some-command"]
+param = ["ssh", "-oStrictHostKeyChecking=no", host, cmd]
+
+ssh = subprocess.Popen(param,
+                       shell=False,
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE)
+result = ssh.stdout.readlines()
+if result == []:
+    error = ssh.stderr.readlines()
+    print(f"ERROR: {error}", file=sys.stderr)
+else:
+    print(result)
