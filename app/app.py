411035da55b5b617e65d6da7ffb0625de3e34d08
chrissssss <ak_2001@gmx.de>
2022-11-24 20:43:51 +0000

    works but needs password

--- app/app.py
+++ app/app.py
@@ -1,27 +1,19 @@
 import subprocess
 import sys
 
-print("Python running!")
-
 user = "test"
 host = "mysshserver"
 cmd = "date"
 
-cmdline = f"ssh -oStrictHostKeyChecking=no {user}@{host} {cmd}"
-
-
-print("Python finished!")
-
-command = "uname -a"
-
-# ["ssh", "some-host@some-user", "some-command"]
-param = ["ssh", "-oStrictHostKeyChecking=no", host, cmd]
+param = ["ssh", "-v", "-oStrictHostKeyChecking=no", f"{user}@{host}", cmd]
 
 ssh = subprocess.Popen(param,
                        shell=False,
                        stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE)
+
 result = ssh.stdout.readlines()
+
 if result == []:
     error = ssh.stderr.readlines()
     print(f"ERROR: {error}", file=sys.stderr)
